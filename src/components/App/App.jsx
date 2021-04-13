cc11adb37211a9756cc3a02125e2cf96a3c91aad
Kirill Sychevnik <trasco006@gmail.com>
2021-04-13 23:17:36 +0300

    init push phase 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./App.css"
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -10,43 +10,152 @@ import Profile from "../Profile/Profile";
 import {Route, Switch} from 'react-router-dom'
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ProtectedRoute from "../ProtectedRoute";
+import api from "../../utils/MainApi";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [isLoginPageIsOpen, setOpenLoginPage] = useState(() => {
+    return window.location.pathname === '/sign-in';
+  })
+  const [isRegistrationPageIsOpen, setOpenRegistrationPage] = useState(() => {
+    return window.location.pathname === '/sign-up';
+  })
+
+  const [loggedIn, setIsLogin] = useState(false);
   const [filterCheckboxValue, setFilterChechboxValue] = useState(false)
 
+  /**
+   * Эффект при изменении жизненного цикла
+   */
+  useEffect(() => {
+    handleTokenCheck()
+  })
+
+  /**
+   * Ручное закрытие всех окон
+   */
+  const handleCloseAllPages = () => {
+    setOpenLoginPage(false)
+    setOpenRegistrationPage(false)
+  }
+
+  /**
+   * Ручное открытие окна логина
+   */
+  const handleOpenLoginPage = () => {
+    handleCloseAllPages()
+    setOpenLoginPage(true)
+  }
+
+  /**
+   * Ручное открытие окна регистрации
+   */
+  const handleOpenRegistrationPage = () => {
+    handleCloseAllPages()
+    setOpenRegistrationPage(true)
+  }
+
+  /**
+   * Ручная проверка валидности токена
+   */
+  const handleTokenCheck = () => {
+    if (localStorage.getItem('jwt')) {
+      const jwt = localStorage.getItem('jwt');
+      api.checkToken(jwt).then((res) => {
+        setIsLogin(true)
+      })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  }
+
+  /**
+   * Запрос к серверу с параметрами входа, возвращает токен и проверяет его
+   * @param obj - {email : "email", password: "password"}
+   */
+  const handleLogIn = (obj) => {
+    api.signIn(obj)
+      .then((res) => {
+          setIsLogin(true)
+          localStorage.setItem('jwt', res.token);
+          handleTokenCheck();
+        }
+      )
+      .catch(() => {
+        setIsLogin(false)
+      })
+  }
+
+  /**
+   * Удаляет токен из localstorage, устанавливает isLogin на flase
+   */
+  const handleLogOut = () => {
+    localStorage.removeItem('jwt')
+    setIsLogin(false)
+  }
+
+  /**
+   * Делает запрос к серверу с параметрами для регистрации
+   * @param obj
+   */
+  const handleRegister = (obj) => {
+    api.signUp(obj)
+      .then(res => {
+        alert('Вы успешно зарегистрированы!')
+      })
+  }
+
+  /**
+   * Ручной переключатель чекбокса
+   * @param item
+   */
   const handleSetFilterCheckboxValue = (item) => {
     setFilterChechboxValue(!filterCheckboxValue)
   }
-  const handleLogin = () => {
-    setLoggedIn(!loggedIn)
-  }
-  console.log(filterCheckboxValue)
+
   return (
     <div className="app">
       <div className="page">
         <Switch>
-          <Route path="/sign-in">
-            <Login/>
-          </Route>
+          {(loggedIn !== true) ? (<Route path="/sign-in">
+            <Login isLogin={loggedIn}
+                   handleOpenRegistrationPage={handleOpenRegistrationPage}
+                   handleLogIn={handleLogIn}/>
+          </Route>) : null}
+
+          {(loggedIn !== true) ? (<Route path="/sign-up">
+            <Register
+              handleRegister={handleRegister}
+              handleOpenLoginPage={handleOpenLoginPage}
+              isLogin={loggedIn}/>
+          </Route>) : null}
+
           <Route path="/movies">
-            <Movies handleCheckboxSet={handleSetFilterCheckboxValue}/>
+            <ProtectedRoute component={Movies}
+                            handleCheckboxSet={handleSetFilterCheckboxValue}
+                            isLogin={loggedIn}/>
           </Route>
+
           <Route path="/saved-movies">
-            <Movies handleCheckboxSet={handleSetFilterCheckboxValue}/>
+            <ProtectedRoute component={Movies}
+                            handleCheckboxSet={handleSetFilterCheckboxValue}
+                            isLogin={loggedIn}/>
           </Route>
-          <Route path="/sign-up">
-            <Register/>
-          </Route>
+
           <Route path="/profile">
-            <Header handleLoggin={handleLogin} loggedIn={loggedIn}/>
-            <Profile/>
+            <Header handleLoggin={handleLogIn}
+                    loggedIn={loggedIn}/>
+            <ProtectedRoute component={Profile}
+                            isLogin={loggedIn}/>
           </Route>
+
           <Route path="/error">
             <Error/>
           </Route>
+
           <Route path="/">
-            <Header handleLoggin={handleLogin} loggedIn={loggedIn}/>
+            <Header handleLoggin={handleLogIn} loggedIn={loggedIn}/>
             <Main/>
             <Footer/>
           </Route>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,46 @@
 import './Login.css'
+import {useState} from "react";
 
-const Login = () => {
+const Login = (props) => {
+
+  /**
+   * Объект с данными из инпутов формы
+   */
+  const [inputData, setInputData] = useState({})
+
+  /**
+   * Берёт данные из инпута и записывает в стэйт
+   * @param input
+   */
+  const getDataFromInput = (input) => {
+    inputData[input.target.id] = input.target.value
+  }
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleLogIn(inputData)
+  }
+
+
   return (
     <div className='login'>
       <div className="login__header">
         <div className="logo"/>
         <h1 className="login__title">Рады видеть!</h1>
       </div>
-      <form className="login__form">
+      <form onSubmit={handleSubmit} className="login__form">
         <div className="login__form-container">
           <p className="login__form-input-prompt">E-mail</p>
-          <input type="text"  className="login__form-input"/>
+          <input onChange={getDataFromInput}
+                 type="text" id='email'
+                 className="login__form-input"/>
           <p className="login__form-input-prompt">Пароль</p>
-          <input type="text" className="login__form-input"/>
+          <input onChange={getDataFromInput}
+                 type="password" id='password'
+                 className="login__form-input"/>
         </div>
         <button type="submit" className="login__submit-button">Войти</button>
       </form>
-      <p className="login__reg-faq">Ещё не зарегистрированы? <a href="#" className="login__reg-link">Регистрация</a></p>
+      <p className="login__reg-faq">Ещё не зарегистрированы? <a href="/sign-up" className="login__reg-link">Регистрация</a></p>
     </div>
   )
 }
